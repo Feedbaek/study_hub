56203d707fef5c3827cd4cc0fa2bc07b7b34a708
feedbaek <nicek789@naver.com>
2022-07-22 01:29:38 +0900

    자바 스터디 추가

---
+++ java_study/22.07.27 - 2주차/Factory_pattern/src/Car.java
@@ -0,0 +1,13 @@
+public class Car {
+	public static final String SONATA = "Sonata";
+	public static final String GRANDEUR = "Grandeur";
+	public static final String GENESIS = "Genesis";
+	String tmp;
+	String productName;
+	public Car(String productName) {
+		this.productName = productName;
+	}
+	public String toString() {
+		return productName;
+	}
+}
